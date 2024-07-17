2be3aaf756b60307ef5f31f30acd267d0402218b
Mirza Subasic <Mirza.Subasic@comtradeintegration.onmicrosoft.com>
2024-07-17 14:17:58 +0200

    Osnovna konfiguracija ExtentReporta

--- Automatski-Testovi/Tests/DriversSetup.cs
+++ Automatski-Testovi/Tests/DriversSetup.cs
@@ -1,5 +1,7 @@
 ﻿using Automatski_Testovi.Pages;
 using Automatski_Testovi.Static_elements;
+using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Edge;
@@ -9,6 +11,11 @@ namespace Automatski_Testovi.Tests
     public class DriversSetup
     {
         public IWebDriver driver;
+
+        private static ExtentReports extent;
+        private static ExtentHtmlReporter htmlReporter;
+        public ExtentTest test;
+
         public LoginPage? loginPage;
         public InventoryPage? inventoryPage;
         public CartPage? cartPage;
@@ -22,19 +29,16 @@ namespace Automatski_Testovi.Tests
         {
             ChromeDriverSetUp();
 
-            loginPage = new LoginPage(driver);
-            inventoryPage = new InventoryPage(driver);
-            cartPage = new CartPage(driver);
-            checkoutStepOnePage = new CheckoutStepOnePage(driver);
-            checkoutStepTwoPage = new CheckoutStepTwoPage(driver);
-            checkoutCompletePage = new CheckoutCompletePage(driver);
-            staticData = new StaticData();
+            ExtentReportsSetUp();
+
+            InitializeClasses();
         }
 
         [OneTimeTearDown]
         public void TearDown()
         {
             driver?.Dispose();
+            ExtentReportsTearDown();
         }
 
         private void ChromeDriverSetUp()
@@ -52,5 +56,35 @@ namespace Automatski_Testovi.Tests
             options.AddArgument("--start-maximized");
             driver = new EdgeDriver(options);
         }
+
+        private void InitializeClasses()
+        {
+            loginPage = new LoginPage(driver);
+            inventoryPage = new InventoryPage(driver);
+            cartPage = new CartPage(driver);
+            checkoutStepOnePage = new CheckoutStepOnePage(driver);
+            checkoutStepTwoPage = new CheckoutStepTwoPage(driver);
+            checkoutCompletePage = new CheckoutCompletePage(driver);
+            staticData = new StaticData();
+        }
+
+        private void ExtentReportsSetUp()
+        {
+            string downloadsFolder = "C:\\Users\\USER_NAME\\Downloads\\".Replace("USER_NAME", Environment.UserName);
+
+            var htmlReporter = new ExtentV3HtmlReporter(downloadsFolder + "ExentReport.html");
+
+            extent = new ExtentReports();
+
+            extent.AttachReporter(htmlReporter);
+
+            test = extent.CreateTest(TestContext.CurrentContext.Test.Name);
+        }
+
+        private void ExtentReportsTearDown()
+        {
+            extent.Flush();
+        }
+
     }
 }
